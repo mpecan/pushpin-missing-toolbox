6ca6f21359383596df6a89e31476d99e653d7346
Matjaz Domen Pecan <matjaz.pecan@gmail.com>
2025-04-15 19:34:41 +0200

    feat: rename files for improved structure and clarity in the project

--- discovery/build.gradle.kts
+++ discovery/build.gradle.kts
@@ -1,20 +1,24 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.4"
+    id("org.springframework.boot") version "3.4.4" apply false
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.mpecan"
 version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
-}
-
 repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
