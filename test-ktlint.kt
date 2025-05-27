e801c394bb3fce4854720a51fe69e07456839256
Matjaz Domen Pecan <matjaz.pecan@gmail.com>
2025-05-27 18:24:48 +0200

    chore: remove test file

--- test-ktlint.kt
+++
@@ -1,9 +0,0 @@
-package test
-
-import java.util.*  // Wildcard import - ktlint violation
-
-class TestKtlint {
-    fun test() {
-        println("This line is way too long and will exceed the maximum line length of 120 characters which should trigger a ktlint violation")
-    }
-}
